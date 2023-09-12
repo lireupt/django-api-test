93ded49ee6fee73a65b223d2361b102664d69173
lireupt <lireu.pt@gmail.com>
2023-09-12 22:08:47 +0100

    house keeping

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -10,9 +10,9 @@ from django.db.utils import  OperationalError
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
-    """Django command to wait for the database."""
+    """Django command to wait for database."""
 
-    def handle(self,*args,**options):
+    def handle(self, *args, **options):
         """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
         db_up = False
