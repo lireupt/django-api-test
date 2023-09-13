9b9c6f089fb1d3a6e26cb9a5396062c8a8c1d245
lireupt <lireu.pt@gmail.com>
2023-09-13 20:16:19 +0100

    a

--- app/app/tests.py
+++ app/app/tests.py
@@ -10,12 +10,12 @@ class CalTests(SimpleTestCase):
 # Test calc module
     def test_add_calculator(self):
         # Test the calculator method
-        res = calc.add(5,5)
+        res = calc.add(5, 5)
 
         self.assertEqual(res, 10)
 
     def test_subtract_number(self):
         # Test subtracting numbers
-        res = calc.subtract(10,15)
+        res = calc.subtract(10, 15)
 
         self.assertEqual(res, -5)
